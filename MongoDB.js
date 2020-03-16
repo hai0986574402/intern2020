622a824dfff8c9a7a664a6ffb33d6ecca8945c73
Nguyễn Thanh Hải <50979816+hai0986574402@users.noreply.github.com>
2020-03-16 11:49:08 +0700

    Delete MongoDB.js

--- MongoDB.js
+++
@@ -1,7 +0,0 @@
-var mongoClient = require('mongodb').MongoClient;
-mongoClient.connect('mongodb://127.0.0.1:27017', function(err, db) {
-    if (err) throw err;
-    console.log('Ket noi thanh cong');
-    db.close();
-    console.log('close thanh cong');
-});
